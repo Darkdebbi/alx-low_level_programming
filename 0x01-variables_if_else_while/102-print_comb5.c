14550c051d017174126314127a1ab1be7031acff
Darkdebbi <akintumileabimbola@gmail.com>
2022-09-12 17:03:04 -0700

    software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - program that prints all possible combinations of two two-digit numbers
+* Return: 0
+ */
+int main(void)
+
+
+{
+while (a <= 99)
+	b = a + 1;
+
+	while (b <= 90)
+
+		while (b <= 99)
+
+		{
+
+			putchar((a / 10) + n1);
+
+			putchar((a % 10) + n1);
+}
