ea6f47e1c85e3ddc62ae07493195dcd260e26a80
Darkdebbi <akintumileabimbola@gmail.com>
2022-10-14 04:43:49 -0700

    value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "varidic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers, followed by a new line.
+ * @seperator: The string to be printed between numbers.
+ * @n: The number of intergers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
+{
+	va_list nums;
+	unsigned int index;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && seperator != NULL)
+			printf("%d", seperator);
+	}
+
+	printf("\n";
+
+	va_end(nums);
+}
